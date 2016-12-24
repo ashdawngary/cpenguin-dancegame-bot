6361adc15fdde03d004c6b4be5c10a84d32867ad
ashdawngary <ashdawngary@gmail.com>
2016-12-24 16:36:10 -0500

    Statrt
    starting project

---
+++ imagebase.py
@@ -0,0 +1,25 @@
+'''
+imagebase.py
+Has all core features for handling windows 10 output and screen input.
+'''
+from PIL import ImageGrab
+from PIL import ImageOps
+from numpy import *
+import win32api,win32con
+import numpy
+import os
+import time
+
+def getPixelatmouse():
+    im = ImageGrab.grab()
+    return im.getpixel(getcords())
+def mousePos(cord):
+    win32api.SetCursorPos(cord)
+
+def leftClick():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(.1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+ 
+def getcords():
+    return win32api.GetCursorPos()
